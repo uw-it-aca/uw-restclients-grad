1cea226edea7311979fc69ec927ec7ee7a9eed17
Fang Lin <fanglin@uw.edu>
2017-06-16 11:23:26 -0700

    setup pypi

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     license='Apache License, Version 2.0',
     description=('A library for connecting to the Grad API'),
     long_description=README,
-    url="https://github.com/uw-it-aca/uw-restclients-iasystem",
+    url="https://github.com/uw-it-aca/uw-restclients-grad",
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
