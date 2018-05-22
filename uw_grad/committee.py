66b5228ebb9fd51923cdc64b317f44123d810848
Fang Lin <fanglin@uw.edu>
2018-05-22 14:33:12 -0700

    Use PWS 2.0

--- uw_grad/committee.py
+++ uw_grad/committee.py
@@ -4,8 +4,7 @@ Interfacing with the Grad Scho Committee Request API
 import logging
 import json
 from uw_grad.models import GradCommitteeMember, GradCommittee
-from uw_pws import PWS
-from uw_grad import get_resource, parse_datetime
+from uw_grad import get_resource, parse_datetime, UWPWS
 
 
 PREFIX = "/services/students/v1/api/committee?id="
@@ -19,7 +18,7 @@ def get_committee_by_regid(regid):
     """
     raise: InvalidRegID, DataFailureException
     """
-    person = PWS().get_person_by_regid(regid)
+    person = UWPWS.get_person_by_regid(regid)
     return get_committee_by_syskey(person.student_system_key)
 
 
