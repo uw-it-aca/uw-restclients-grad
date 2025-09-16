8723cd0c02445a381f61b320b43d3459353a5d1f
Fang Lin <fanglin@uw.edu>
2025-09-16 14:34:51 -0700

    Python 3.12 update

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright 2024 UW-IT, University of Washington
+# Copyright 2025 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
 import os
@@ -21,11 +21,10 @@ setup(
     name='UW-RestClients-Grad',
     version=VERSION,
     packages=['uw_grad'],
-    author="UW-IT T&LS",
+    author="UW-IT SETS",
     author_email="aca-it@uw.edu",
     include_package_data=True,
-    install_requires=['UW-RestClients-Core',
-                      'python-dateutil'
+    install_requires=['UW-RestClients-Core'
                       ],
     license='Apache License, Version 2.0',
     description=('A library for connecting to the Grad API'),
